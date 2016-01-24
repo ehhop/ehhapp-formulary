4bbfe0abd469b999a8a4b017122b54190dfed89a
daviszhou <zhou.davis@outlook.com>
2016-01-24 17:43:20 -0500

    Add html template

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 from __future__ import print_function
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def homepage():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
+	port = int(os.environ.get('PORT', 5050))
 	app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
